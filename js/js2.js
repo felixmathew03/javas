5845af0fcc91939668465b8a49d0af35462b60d7
felixmathew03 <felixmathewtt@gmail.com>
2024-07-15 09:20:09 +0530

    mm

---
+++ js/js2.js
@@ -0,0 +1,61 @@
+// for(let i=1; i<=5 ; i++)
+// {
+//     for(let j=1; j<=5; j++)
+//     {
+//         console.log(j);
+//     }
+// }
+// for(let i=1; i<=5 ; i++)
+//     {
+//         str="";
+//         for( j=1; j<=5; j++)
+//         {
+//             str+=j;
+//         }
+//         console.log(str);
+//     }
+// for( i=1; i<=5 ; i++)
+// {
+//     str="";
+//     for( j=1; j<=5; j++)
+//     {
+//         str+=i;
+//     }
+//     console.log(str);
+// }
+// for( i=1; i<=5 ; i++)
+//     {
+//         str="";
+//         for( j=1; j<=i; j++)
+//         {
+//             str+=j;
+//         }
+//         console.log(str);
+//     }
+// for( i=1; i<=5 ; i++)
+//     {
+//         str="";
+//         for( j=1; j<=i; j++)
+//         {
+//             str+=i;
+//         }
+//         console.log(str);
+//     }
+// for( i=5; i>=1 ; i--)
+// {
+//     str="";
+//     for( j=1; j<=i; j++)
+//     {
+//         str+=j;
+//     }
+//     console.log(str);
+// }
+for( i=1; i<=5 ; i++)
+    {
+        str="";
+        for( j=i; j<=i; j++)
+        {
+            str+=j;
+        }
+        console.log(str);
+    }
