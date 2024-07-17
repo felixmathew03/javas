62d4c8f826c0964cc9803908ea8d586010dcf1bc
felixmathew03 <felixmathewtt@gmail.com>
2024-07-17 09:07:49 +0530

    yeye

---
+++ js/array.js
@@ -0,0 +1,21 @@
+// a=[1,2,3,"hai","hello","good",true,false];
+// console.log(a);
+// console.log(a[5]);
+// console.log(a.length);
+// console.log(a.indexOf("good"));
+// a.push("mern");  //add data at end of the array
+// console.log(a);
+//  a.pop();    //delete data from end of array
+//  console.log(a);
+// a.shift();   //delete data from beginning of array
+// console.log(a);
+// a.splice(a.indexOf(3),1);    //delete a particular index element
+// console.log(a);
+// console.log(a.toString());
+// console.log(a.reverse());
+// console.log(a.join(" "));
+
+// j=0;
+// for(i=1;i<=100;i++){
+//     if()
+// }
---
+++ js/string.js
@@ -0,0 +1,29 @@
+// str="hello-world-have-a-good-day"
+// console.log(str);
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase().toLowerCase().length);
+// console.log(str.trim());
+// console.log(str.trimLeft());
+// console.log(str.trimRight());
+// console.log(str.substring(0,10));
+// console.log(str.replace("good","nice"));
+// console.log(str.startsWith("hell"));
+// console.log(str.endsWith("das"));
+// arr=str.split("-");
+// console.log(arr);
+// st=arr.join(" ");
+// console.log(st);
+str="hello world have a good day"
+ar=str.split(" ");
+console.log(ar);
+console.log(ar.reverse());
+str=ar.join(" ");
+console.log(str);
+// for(i=0;i<ar.length;i++){
+
+//     console.log(ar[i]);
+    
+
+// }
