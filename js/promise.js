7f1286d14319c5a69940cde7ae0c3118863cf8c1
felixmathew03 <felixmathewtt@gmail.com>
2024-08-16 08:50:20 +0530

    g

--- js/promise.js
+++ js/promise.js
@@ -18,17 +18,45 @@
 
 //promise chaining 
 
-function fetchData(){
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res)=>{
-        return res.json();
-    })
-    .then((data)=>{
-        console.log(data);
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-}
+// function fetchData(){
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((res)=>{
+//         return res.json();
+//     })
+//     .then((data)=>{
+//         console.log(data);
+//     })
+//     .catch((error)=>{
+//         console.log(error);
+//     })
+// }
 
-fetchData();
+// fetchData();
+
+
+// fetch("https://jsonplaceholder.typicode.com/todos").then((res)=>{
+//     if(res.status==200)
+//         return res.json()
+//     else{
+//         alert("data not found");
+//     }
+// })
+// .then((data)=>{
+//     console.log(data);
+// })
+// .catch((error)=>{
+//     alert("network issue");
+//     console.log(error);
+// })
+async function fetchData(){
+    try{
+        const res=await fetch("https://jsonplaceholder.typicode.com/todos");
+        if(res.status!=200)
+            return alert("api error");
+        const data=await res.json();
+        console.log(data);
+    }catch(error){
+        console.log(error);
+    }
+}
+fetchData()
